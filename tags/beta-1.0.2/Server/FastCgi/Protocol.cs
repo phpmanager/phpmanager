5cb85e0825a1c801a9ad3c71054d3ae5aec61214
Ruslan Yakushev <ruslany@microsoft.com>
2010-08-29 15:24:35 +0000

    git-svn-id: https://phpmanager.svn.codeplex.com/svn@2312 91bfc952-fec6-4be4-b192-9eb3b8389945

---
+++ tags/beta-1.0.2/Server/FastCgi/Protocol.cs
@@ -0,0 +1,21 @@
+//-----------------------------------------------------------------------
+// <copyright>
+// Copyright (C) Ruslan Yakushev for the PHP Manager for IIS project.
+//
+// This file is subject to the terms and conditions of the Microsoft Public License (MS-PL).
+// See http://www.microsoft.com/opensource/licenses.mspx#Ms-PL for more details.
+// </copyright>
+//----------------------------------------------------------------------- 
+
+
+using System;
+
+namespace Web.Management.PHP.FastCgi
+{
+
+    internal enum Protocol
+    {
+        NamedPipe = 0,
+        Tcp = 1
+    }
+}
---
+++ tags/beta-1.0.2/Setup/PHPManagerSetupHelper/SetupAction.cs
@@ -0,0 +1,40 @@
+//-----------------------------------------------------------------------
+// <copyright>
+// Copyright (C) Ruslan Yakushev for the PHP Manager for IIS project.
+//
+// This file is subject to the terms and conditions of the Microsoft Public License (MS-PL).
+// See http://www.microsoft.com/opensource/licenses.mspx#Ms-PL for more details.
+// </copyright>
+//----------------------------------------------------------------------- 
+
+using System.ComponentModel;
+using System.Configuration.Install;
+using System.Reflection;
+
+namespace Web.Management.PHP.Setup
+{
+
+    [RunInstaller(true)]
+    public class SetupAction : Installer
+    {
+
+        public override void Install(System.Collections.IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            AssemblyName assemblyName = assembly.GetName();
+            string assemblyFullName = assemblyName.FullName;
+            string clientAssemblyFullName = assemblyFullName.Replace(assemblyName.Name, "Web.Management.PHP");
+
+            InstallUtil.AddUIModuleProvider("PHP", "Web.Management.PHP.PHPProvider, " + clientAssemblyFullName);
+        }
+
+        public override void Uninstall(System.Collections.IDictionary savedState)
+        {
+            base.Uninstall(savedState);
+
+            InstallUtil.RemoveUIModuleProvider("PHP");
+        }
+    }
+}
