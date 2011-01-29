04a1d0d507ce7ae557844a55373dc0a7d5d878bd
Ruslan Yakushev <ruslany@microsoft.com>
2011-01-29 08:26:11 +0000

    Updating the version to 1.2.0 and changing some assembly attributes.
    git-svn-id: https://phpmanager.svn.codeplex.com/svn@6315 91bfc952-fec6-4be4-b192-9eb3b8389945

--- trunk/SharedAssembyInfo.cs
+++ trunk/SharedAssembyInfo.cs
@@ -12,9 +12,9 @@ using System.Reflection;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyCompany("RuslanY.net")]
+[assembly: AssemblyCompany("PHP Manager Project")]
 [assembly: AssemblyProduct("PHP Manager for IIS")]
-[assembly: AssemblyCopyright("Copyright © Ruslan Yakushev 2010")]
+[assembly: AssemblyCopyright("Copyright © PHP Manager Project 2010")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -28,5 +28,5 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1.0")]
-[assembly: AssemblyFileVersion("1.1.1.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
