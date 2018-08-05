db6bbf9c9b22feac98984a00f16e7ca7e4a51489
Lex Li <support@lextm.com>
2018-08-04 21:06:18 -0400

    Enabled WiX bitness check.

--- build.installer.bat
+++ build.installer.bat
@@ -1,1 +1,6 @@
-%WINDIR%\Microsoft.NET\Framework\v3.5\msbuild .\Setup\PHPManagerForIIS.wixproj /property:Configuration=Release /property:TargetPlatform=x86
+del bin\Release\PHPManagerForIIS.msi
+%WINDIR%\Microsoft.NET\Framework\v3.5\msbuild .\Setup\PHPManagerForIIS.wixproj /t:Rebuild /property:Configuration=Release /property:Platform=x86
+copy bin\Release\PHPManagerForIIS.msi .\PHPManagerForIIS_x86.msi
+del bin\Release\PHPManagerForIIS.msi
+%WINDIR%\Microsoft.NET\Framework\v3.5\msbuild .\Setup\PHPManagerForIIS.wixproj /t:Rebuild /property:Configuration=Release /property:Platform=x64
+copy bin\Release\PHPManagerForIIS.msi .\PHPManagerForIIS_x64.msi
--- pack.net4x.bat
+++
@@ -1,3 +0,0 @@
-CALL "%VS120COMNTOOLS%\vsvars32.bat"
-CALL devenv .\Setup\PHPManagerSetup8.vdproj /build %1
-CALL devenv .\Setup\PHPManagerSetup8_64.vdproj /build %1
