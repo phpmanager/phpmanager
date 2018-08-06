323998c76e81891727ca6a256fa7faa6c9582c1a
Lex Li <support@lextm.com>
2018-08-05 22:35:14 -0400

    Revised the product name.

--- sign.bat
+++ sign.bat
@@ -1,2 +1,2 @@
 set signtool="C:\Program Files (x86)\Windows Kits\10\bin\10.0.17134.0\x64\signtool.exe"
-if exist f:\sign.txt (%signtool% sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /a .\*.msi)
+if exist f:\sign.txt (%signtool% sign /tr http://timestamp.digicert.com /td sha256 /fd sha256 /d "PHP Manager 2 for IIS" /a .\*.msi)
