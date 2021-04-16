be7571be6e0f3417a5d96dfc266df17ae2a5fdc5
Lex Li <support@lextm.com>
2021-04-16 16:08:26 -0400

    Updated choco files.

--- Chocolatey/tools/chocolateyInstall.ps1
+++ Chocolatey/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 Install-ChocolateyPackage -PackageName 'PHP Manager 2 for IIS' -FileType 'msi' -SilentArgs '/quiet' `
--File 'https://github.com/phpmanager/phpmanager/releases/download/v2.5/PHPManagerForIIS_x86.msi' `
--File64 'https://github.com/phpmanager/phpmanager/releases/download/v2.5/PHPManagerForIIS_x64.msi' `
--Checksum '4F81FA98E18B64D1388387BC8E534D8E41F28F39' -ChecksumType 'sha1' `
--Checksum64 'C0550804E601402A7DC607101C4D01B571D6BF9A' -ChecksumType64 'sha1'
+-File 'https://github.com/phpmanager/phpmanager/releases/download/v2.6/PHPManagerForIIS_x86.msi' `
+-File64 'https://github.com/phpmanager/phpmanager/releases/download/v2.6/PHPManagerForIIS_x64.msi' `
+-Checksum 'BBAB59DF0800E0CD35A1C2CA19D9773E5C20DC8E' -ChecksumType 'sha1' `
+-Checksum64 '7FF3504E7DB1DB309AC5C290B6CBB7E2CFF6D006' -ChecksumType64 'sha1'
