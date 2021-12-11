db6720d59ff5b32b4bb58c59238c4ccf26eaeb1c
Lex Li <support@lextm.com>
2021-12-11 06:08:45 -0800

    Updated choco files.

--- Chocolatey/tools/chocolateyInstall.ps1
+++ Chocolatey/tools/chocolateyInstall.ps1
@@ -1,5 +1,5 @@
 Install-ChocolateyPackage -PackageName 'PHP Manager 2 for IIS' -FileType 'msi' -SilentArgs '/quiet' `
--File 'https://github.com/phpmanager/phpmanager/releases/download/v2.6/PHPManagerForIIS_x86.msi' `
--File64 'https://github.com/phpmanager/phpmanager/releases/download/v2.6/PHPManagerForIIS_x64.msi' `
--Checksum 'BBAB59DF0800E0CD35A1C2CA19D9773E5C20DC8E' -ChecksumType 'sha1' `
--Checksum64 '7FF3504E7DB1DB309AC5C290B6CBB7E2CFF6D006' -ChecksumType64 'sha1'
+-File 'https://github.com/phpmanager/phpmanager/releases/download/v2.7/PHPManagerForIIS_x86.msi' `
+-File64 'https://github.com/phpmanager/phpmanager/releases/download/v2.7/PHPManagerForIIS_x64.msi' `
+-Checksum '9AE196C06BE98C044C1A0BB9D85C6D0729EAAE21' -ChecksumType 'sha1' `
+-Checksum64 '25EA786A271C9754DF33BE86E638C4E1EA3EAD63' -ChecksumType64 'sha1'
---
+++ dist.sign.bat
@@ -0,0 +1,2 @@
+powershell -ExecutionPolicy Bypass -file sign.ps1
+powershell -ExecutionPolicy Bypass -file sha1.ps1
