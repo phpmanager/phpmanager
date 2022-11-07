cd5f6710fbbb6c128d0250ed922eb81ffbb4c70d
Lex Li <support@lextm.com>
2022-11-07 01:11:52 -0500

    Added script validation.

--- dist.debug.bat
+++ dist.debug.bat
@@ -1,2 +1,10 @@
 rmdir /S /Q bin
 CALL build.net35.bat Debug
+IF %ERRORLEVEL% NEQ 0 goto failed
+
+echo succeeded.
+exit /b 0
+
+:failed
+echo failed.
+exit /b 1
--- dist.release.bat
+++ dist.release.bat
@@ -1,6 +1,17 @@
 rmdir /S /Q bin
 CALL build.net35.bat Release
-powershell -file sign.ps1
+powershell -ExecutionPolicy Bypass -file sign.ps1
 CALL build.installer.bat
-powershell -file sign.installers.ps1
-powershell -file sha1.ps1
+powershell -ExecutionPolicy Bypass -file sign.installers.ps1
+IF %ERRORLEVEL% NEQ 0 goto failed
+powershell -ExecutionPolicy Bypass -file verify.ps1
+IF %ERRORLEVEL% NEQ 0 goto failed
+powershell -ExecutionPolicy Bypass -file sha1.ps1
+IF %ERRORLEVEL% NEQ 0 goto failed
+
+echo succeeded.
+exit /b 0
+
+:failed
+echo failed.
+exit /b 1
